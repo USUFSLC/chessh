e33c52f97b51b05bb9509035c22c9eed3fc13a11
Simponic <loganhunt@simponic.xyz>
2023-01-18 10:07:40 -0700

    Add middleware proxy on dev

---
+++ front/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  if (process.env.NODE_ENV != 'production') {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      })
+    );
+  }
+};
